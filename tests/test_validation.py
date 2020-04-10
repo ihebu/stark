6cfc0f28fb223f99b8eae8d3f4e6b1ffdf48d3ac
Iheb Haboubi <iheb.haboubi56@gmail.com>
2020-04-10 19:53:44 +0100

    Parametrize tests

--- tests/test_validation.py
+++ tests/test_validation.py
@@ -3,38 +3,74 @@ import pytest
 from stark.validation import validate_length, validate_logic, validate_types
 
 
-def test_validate_length():
-    assert validate_length(None) == True
-    assert validate_length(5) == True
-    assert validate_length(1) == True
-    with pytest.raises(TypeError):
-        validate_length("5")
-    with pytest.raises(ValueError):
-        validate_length(-2)
-    with pytest.raises(ValueError):
-        validate_length(0)
+@pytest.mark.parametrize(
+    "length, expected",
+    [
+        (None,True),
+        (5,True),
+        (1,True)
+    ]
+)
+def test_validate_length(length,expected):
+    assert validate_length(length) == expected
+
+@pytest.mark.parametrize(
+    "length, expected",
+    [
+        ("5",TypeError),
+        (-2,ValueError),
+        (0,ValueError)
+    ]
+)
+def test_validate_length_exceptions(length,expected):
+    with pytest.raises(expected):
+        validate_length(length)
+    
 
 
-def test_validate_types():
-    with pytest.raises(TypeError):
-        validate_types(2)
-    with pytest.raises(TypeError):
-        validate_types({"lowercase": "5"})
-    with pytest.raises(ValueError):
-        validate_types({"lowercase": False})
-    with pytest.raises(ValueError):
-        validate_types({"other": -2})
-    assert validate_types({"uppercase": 5, "digits": 15}) == True
-    assert validate_types({"uppercase": 5, "digits": True}) == True
+@pytest.mark.parametrize(
+    'types, expected',
+    [
+     ({"uppercase": 5, "digits": 15},True),
+     ({"uppercase": 5, "digits": True},True)   
+    ]
+)
+def test_validate_types(types,expected):
+    assert validate_types(types) == expected
+
+@pytest.mark.parametrize(
+    'types, expected',
+    [
+     (2,TypeError),
+     ({"lowercase": "5"},TypeError),
+     ({"lowercase": False},ValueError),
+     ({"other": -2},ValueError)   
+    ]
+)
+def test_validate_types_exceptions(types,expected):
+    with pytest.raises(expected):
+        validate_types(types)
+
+@pytest.mark.parametrize(
+    'length, types, expected',
+    [
+        (20,{"lowercase": 15, "symbols": True},True),
+        (None, {"lowercase": 15, "symbols": 5},True),
+    ]
+)
+def test_validate_logic(length,types,expected):
+    assert validate_logic(length, types) == expected
 
 
-def test_validate_logic():
-    with pytest.raises(ValueError):
-        validate_logic(15, {"lowercase": 15, "uppercase": True})
-    with pytest.raises(ValueError):
-        validate_logic(20, types={"lowercase": 15, "uppercase": 6})
-    with pytest.raises(TypeError):
-        validate_logic(None, {"lowercase": True})
-        validate_logic(None, {"lowercase": 15, "symbols": True})
-    assert validate_logic(20, {"lowercase": 15, "symbols": True})
-    assert validate_logic(None, {"lowercase": 15, "symbols": 5})
+@pytest.mark.parametrize(
+    'length, types, expected',
+    [
+        (15,{"lowercase": 15, "uppercase": True},ValueError),
+        (20,{"lowercase": 15, "uppercase": 6},ValueError),
+        (None,{"lowercase": True},TypeError),
+        (None, {"lowercase": 15, "symbols": True},TypeError)
+    ]
+)
+def test_validate_logic_exceptions(length,types,expected):
+    with pytest.raises(expected):
+        validate_logic(length, types)
